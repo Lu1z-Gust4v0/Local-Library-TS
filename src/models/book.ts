93d703a8999b563584acb55e86ffb84d48a51ea5
Luiz Gustavo <gustavo1204.gc@gmail.com>
2022-08-10 22:23:15 -0300

    Create book model

---
+++ src/models/book.ts
@@ -0,0 +1,19 @@
+import { model, Schema } from "mongoose"
+import { IBook } from "../types/models"
+
+
+const bookSchema = new Schema<IBook>({
+    title: { type: String, required: true },
+    author: { type: Schema.Types.ObjectId, ref: "Author", required: true},
+    summary: { type: String, required: true},
+    ISBN: { type: String, required: true },
+    genre: [{ type: Schema.Types.ObjectId, ref: "Genre" }]
+})
+
+bookSchema.virtual("url").get(function () {
+    return `/catalog/book/${this._id}`
+})
+
+const Book = model<IBook>("Book", bookSchema)
+
+export default Book
