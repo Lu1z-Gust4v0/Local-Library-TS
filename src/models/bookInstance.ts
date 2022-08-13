28a18838759398a4ac80f0d184ab8d14abecade0
Luiz Gustavo <gustavo1204.gc@gmail.com>
2022-08-13 18:22:03 -0300

    Add a date parser virtual for BookInstance model, update view

--- src/models/bookInstance.ts
+++ src/models/bookInstance.ts
@@ -1,6 +1,6 @@
 import { Schema, model } from "mongoose"
 import { IBookInstance } from "../types/models"
-
+import { format } from "date-fns"
 
 const bookInstanceSchema = new Schema<IBookInstance>({
     book: { type: Schema.Types.ObjectId, ref: "Book", required: true},
@@ -13,6 +13,12 @@ const bookInstanceSchema = new Schema<IBookInstance>({
     },
     dueBack: { type: Date, default: Date.now }
 })
+
+bookInstanceSchema.virtual("formattedDueBack").get(function () {
+    // If dueBack is not defined get a new Date
+    return format(this.dueBack ?? new Date(), "MMM, dd'th'")
+})
+
 
 bookInstanceSchema.virtual("url").get(function () {
     return `/catalog/book-instance/${this._id}`
