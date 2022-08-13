f541c258c3d46607d5bee306de56b59fda1b3503
Luiz Gustavo <gustavo1204.gc@gmail.com>
2022-08-13 16:46:00 -0300

    Implement index route's controller

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,8 +1,30 @@
 import { Request, Response } from "express"
+import Book from "../models/book"
+import BookInstance from "../models/bookInstance"
+import Author from "../models/author"
+import Genre from "../models/genre"
 
 
 export const index = async (req: Request, res: Response): Promise<void> => {
-  res.send('NOT IMPLEMENTED: Site Home Page')
+  try {
+    // Create a data object to be passed to the response
+    const data = {
+      bookCount: await Book.countDocuments({}),
+      bookInstanceCount: await BookInstance.countDocuments({}),
+      bookInstanceAvaliable: await BookInstance.countDocuments({ status: "Available" }),
+      authorCount: await Author.countDocuments({}),
+      genreCount: await Genre.countDocuments({}),
+    }
+
+    res.render("index", { title: "Local Library Home", data: data })
+
+  } catch(error: any) {
+    res.render("index", { 
+      title: "local Library Home", 
+      data: {}, 
+      error: new Error(error.message || "Unknown Error") 
+    })
+  }
 }
 
 // Display list of all books.
