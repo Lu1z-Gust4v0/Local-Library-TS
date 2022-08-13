66851c4251cc6efe7702cae0c4ad29b318e1cce5
Luiz Gustavo <gustavo1204.gc@gmail.com>
2022-08-12 23:26:58 -0300

    Create more functions for authorController

--- src/controllers/authorController.ts
+++ src/controllers/authorController.ts
@@ -11,14 +11,29 @@ export const authorDetail = async (req: Request, res: Response): Promise<void> =
     res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`)
 }
 
+// Handle Author create on GET
+export const authorCreateGet = async (req: Request, res: Response): Promise<void> => {
+    res.send("NOT IMPLEMENTED: Author create GET")
+}
+
 // Handle Author create on POST
 export const authorCreatePost = async (req: Request, res: Response): Promise<void> => {
     res.send("NOT IMPLEMENTED: Author create POST")
 }
 
-// Display Author delete from on GET
+// Display Author delete form on GET
 export const authorDeleteGet = async (req: Request, res: Response): Promise<void> => {
     res.send("NOT IMPLEMENTED: Author delete GET")
+}
+
+// Handle Author delete on POST
+export const authorDeletePost = async (req: Request, res: Response): Promise<void> => {
+    res.send("NOT IMPLEMENTED: Author delete POST")
+}
+
+// Display Author update form on GET
+export const authorUpdateGet = async (req: Request, res: Response): Promise<void> => {
+    res.send("NOT IMPLEMENTED: Author update GET")
 }
 
 // Handle Author update on POST
