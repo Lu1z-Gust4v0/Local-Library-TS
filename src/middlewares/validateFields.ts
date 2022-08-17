e83b2470e7e439687a56fcb7a95d16dcc8e9a182
Luiz Gustavo <gustavo1204.gc@gmail.com>
2022-08-16 23:58:22 -0300

    Add middleware to validate create book form

--- src/middlewares/validateFields.ts
+++ src/middlewares/validateFields.ts
@@ -32,3 +32,23 @@ export const validateCreateAuthor = [
     .isISO8601()
     .toDate(),
 ]
+
+export const validateCreateBook = [
+  body("title", "Title must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("author", "Author must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("summary", "Summary must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("isbn", "ISBN must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("genre.*").escape(),  
+]
